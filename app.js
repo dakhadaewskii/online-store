ed492c129c5b72bb95763f5adb390301706d4f66
dakhadaewskii <91620867+dakhadaewskii@users.noreply.github.com>
2022-10-19 19:57:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,46 @@ window.onscroll = function showHeader() {
         header.classList.remove('head-fixed')
     }
 }
+
+//выбор города
+
+let towns = 'towns';
+let search = document.getElementById(towns);
+
+function selectKaspiysk() {
+    search.innerHTML = 'Каспийск';
+};
+
+function selectMoscow() {
+    search.innerHTML = 'Москва';
+};
+
+function selectSaintPetersburg() {
+    search.innerHTML = 'Санкт-Петербург';
+};
+
+function searchRostovOnDon() {
+    search.innerHTML = 'Ростов-на-Дону';
+};
+
+function searchMachachkala() {
+    search.innerHTML = 'Махачкала';
+};
+
+function searchSaratov() {
+    search.innerHTML = 'Саратов';
+};
+
+function searchNizhnyNovgorod() {
+    search.innerHTML = 'Нижний Новгород';
+};
+
+function searchKhabarovsk() {
+    search.innerHTML = 'Хабаровск';
+};
+
+function searchDerbent() {
+    search.innerHTML = 'Дербент';
+};
+
+//выбор города
